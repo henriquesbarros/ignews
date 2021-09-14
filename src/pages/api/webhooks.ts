1b7129ff92b1edc3331581d183b86edf5fdbc8e0
Henrique Barros <henriquesbarroshs@gmail.com>
2021-09-14 20:42:11 -0300

    listen to events

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next-auth/internals/utils";
+import { Readable } from 'stream';
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+    const chuncks = [];
+
+    for await (const chunk of readable) {
+        chuncks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        );
+    }
+
+    return Buffer.concat(chuncks);
+}
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+const relevantEvents = new Set([
+    'checkout.session.completed'
+])
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        const buf = await buffer(req);
+        const secret = req.headers['stripe-signature'];
+
+        let event: Stripe.Event;
+
+        try {
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+        } catch (err) {
+            return res.status(400).send(`Webhook error: ${err.message}`);
+        }
+
+        const { type } = event;
+
+        if (relevantEvents.has(type)) {
+            console.log('Evento recebido', event);
+        }
+
+        res.status(200).json({ received: true});
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method not allowed');
+    }
+}
